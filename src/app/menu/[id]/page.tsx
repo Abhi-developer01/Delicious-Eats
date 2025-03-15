859e705f0eb9a915c767523557377fccf133a2c6
Abhi-developer01 <theabhishek96@gmail.com>
2025-03-16 00:49:09 +0530

    commit1

--- src/app/menu/[id]/page.tsx
+++ src/app/menu/[id]/page.tsx
@@ -1,19 +1,5 @@
 import ProductDetails from './product-details'
-import type { Metadata } from 'next'
-import { use } from 'react'
 
-type Props = {
-  params: Promise<{ id: string }>
-}
-
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-  return {
-    title: `Menu Item ${params.id}`,
-    description: 'View details about this menu item',
-  }
-}
-
-export default function Page({ params }: Props) {
-  const resolvedParams = use(params)
-  return <ProductDetails id={resolvedParams.id} />
+export default function Page({ params }: { params: { id: string } }) {
+  return <ProductDetails id={params.id} />
 } 
