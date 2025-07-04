85764a7e152a984db6c7b2877fc27bab7843ef3f
Abhi-developer01 <theabhishek96@gmail.com>
2025-07-04 14:18:43 +0530

    favicon

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+    return (
+      <>
+        <title>Delicious Eats - Food Ordering App</title>
+        <link rel="icon" href="/images/logo.png" type="image/png" />
+      </>
+    );
+  }
